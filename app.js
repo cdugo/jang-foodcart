ee3df9ada25221a94d3cc07797938d5d02b4345b
cmurja <55098908+cmurja@users.noreply.github.com>
2020-04-24 18:24:25 -0400

    favicon is added

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
